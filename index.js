0ae1b5f83fd332d6720917d5ea4a30e999c92810
unknown <wbrown152020@gmail.com>
2021-04-29 15:48:08 -0600

    Added Responsiveness to header

---
+++ index.js
@@ -0,0 +1,5 @@
+const burger = document.querySelector(".header__burger");
+const burgerMenu = document.querySelector(".header__navLinks");
+burger.addEventListener("click", () => {
+  //   toggleMenu();
+});
